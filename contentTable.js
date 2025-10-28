a47f32bb4f67338d2fb77fca7aad4eddead405d7
Valso <warut92@gmail.com>
2025-10-28 14:11:28 +0700

    add resize font btn

--- contentTable.js
+++ contentTable.js
@@ -44,3 +44,38 @@
 		document.title = textBetween + "(โน้ตดนตรีไทย ชมรมดนตรีไทย ชสว)";
 	}
 	
+
+ 
+	 const increaseBtn = document.createElement("button");
+	 increaseBtn.className = "increaseBtn";
+	 increaseBtn.textContent = "A+";
+	 document.body.appendChild(increaseBtn);
+ 
+	 const decreaseBtn = document.createElement("button");
+	 decreaseBtn.className = "decreaseBtn";
+	 decreaseBtn.textContent = "A-";
+	 document.body.appendChild(decreaseBtn);
+ 
+	 // === LOAD FONT SIZE FROM localStorage ===
+	 let fontSize = localStorage.getItem("fontSize");
+	 if (fontSize) {
+	   fontSize = parseInt(fontSize);
+	 } else {
+	   fontSize = 20; // default
+	 }
+	document.body.style.fontSize = fontSize + "px";
+ 
+	 // === ADD BUTTON EVENTS ===
+	 increaseBtn.addEventListener("click", () => {
+	   fontSize += 2;
+	  document.body.style.fontSize = fontSize + "px";
+	   localStorage.setItem("fontSize", fontSize);
+	 });
+ 
+	 decreaseBtn.addEventListener("click", () => {
+	   if (fontSize > 8) {
+		 fontSize -= 2;
+		document.body.style.fontSize = fontSize + "px";
+		 localStorage.setItem("fontSize", fontSize);
+	   }
+	 });
